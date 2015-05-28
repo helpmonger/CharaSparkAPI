63dd362678b440dc3fef3d2dc78dbd5c4f623d21
davidhe <beetz12@gmail.com>
2015-05-28 09:29:05 -0400

    updated token expiration to 30 mins

--- services/auth/jwt.js
+++ services/auth/jwt.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 module.exports = function (user, res) {
 	var payload = {
 		sub: user.id,
-		exp: moment().add(1, 'minutes').unix()
+		exp: moment().add(30, 'minutes').unix()
 	}
 
 	var token = jwt.encode(payload, "shhh..");
