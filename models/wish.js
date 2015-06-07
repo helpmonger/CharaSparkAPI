a39350c88905fc9ff00cc85231b11da78b06faba
david <beetz12@gmail.com>
2015-06-07 10:26:55 -0400

    cleaned up wish and added jshint & jscs

--- models/wish.js
+++ models/wish.js
@@ -1,50 +1,55 @@
-  var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
 
-  var WishSchema = new Schema({
-    _wishMaker : { 
-      type: Schema.Types.ObjectId, 
-      ref: 'User' 
+var WishSchema = new Schema({
+
+    _wishMaker: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     },
-    _fulfiller : { 
-      type: Schema.Types.ObjectId, 
-      ref: 'User' 
+    _fulfiller: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     },
     _charity: {
-      type: Schema.Types.ObjectId, 
-      ref: 'Charity' 
+        type: Schema.Types.ObjectId,
+        ref: 'Charity'
     },
-     _donation: {
-      type: Schema.Types.ObjectId, 
-      ref: 'Donation' 
+    _donation: {
+        type: Schema.Types.ObjectId,
+        ref: 'Donation'
     },
     title: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     },
     description: {
-      type: String,
-      required: true
-    },    
-    wishStatus: {
-      type: String,
-      default: 'new'      //other statuses are: pending (accepted by fulfiller, pending wish maker approval) , proceeding, completed, cancelled (cancelled by wishmaker), aborted (cancelled by fulfiller)
+        type: String,
+        required: true
     },
-    createdDate: { 
-      type: Date, 
-      default: Date.now 
+    //statuses:
+    //new
+    //pending (accepted by fulfiller, pending wish maker approval)
+    //proceeding
+    //completed
+    //cancelled (cancelled by wishmaker)
+    //aborted (cancelled by fulfiller)
+
+    wishStatus: {
+        type: String,
+        default: 'new'
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
     },
     location: [Number],
-    startDate: { 
-      type: Date
+    startDate: {
+        type: Date
     },
-    expireDate: { 
-      type: Date
+    expireDate: {
+        type: Date
     }
-  });
+});
 
-  WishSchema.methods.hasPaid = function (password, callback) {
-  bcrypt.compare(password, this.password, callback);
-}
-
-  mongoose.model('Wish', WishSchema);
+mongoose.model('Wish', WishSchema);
--- routes.js
+++ routes.js
@@ -2,100 +2,81 @@ var passport = require('passport');
 var createSendToken = require('./services/auth/jwt');
 var flash = require('connect-flash');
 
-module.exports = function(app){
-	
+module.exports = function(app) {
 
-	
-	// var users = require('./controllers/users');
-	// app.get('/users', users.findAll);
-	// app.post('/users', users.add);
+    var PATH = '/api/';
 
+    var versionNo = '0.0.1';
 
-	var PATH = '/api/'
+    // var Auth = require('./controllers/auth');
+    // app.post(PATH + 'register', Auth.SignUp);
+    // app.post(PATH +'login', Auth.Login);
 
-	var versionNo = '0.0.1';
+    app.post(PATH + 'register', passport.authenticate('local-register', {
+        failureFlash: true
+    }), function(req, res) {
+        var foo = req.flash();
+        console.log('foo is ', foo);
+        console.log('in register');
+        createSendToken(req.user, res);
+    });
 
+    app.post(PATH + 'login', passport.authenticate('local-login'), function(req, res) {
+        console.log('in login');
+        createSendToken(req.user, res);
+    });
 
-	// var Auth = require('./controllers/auth');
-	// app.post(PATH + 'register', Auth.SignUp);
-	// app.post(PATH +'login', Auth.Login);
+	var Test = require('./controllers/test');
+    app.get(PATH + 'test', Test.hello);
 
-	
-	app.post(PATH + 'register', passport.authenticate('local-register', 
-								   { failureFlash: true }), function (req, res) {
-		var foo = req.flash();
-		console.log('foo is ', foo);
-		console.log('in register');
-		createSendToken(req.user, res);
-	});
-	
+    var Braintree = require('./controllers/braintree');
+    app.get(PATH + 'token', Braintree.getToken);
+    app.post(PATH + 'processPayment', Braintree.processPayment);
 
-	app.post(PATH + 'login', passport.authenticate('local-login'), function (req, res) {
-		console.log('in login');
-		createSendToken(req.user, res);
-	});
-
-
-	var Braintree = require('./controllers/braintree');
-	app.get(PATH + 'token', Braintree.getToken);
-	app.post(PATH +'processPayment', Braintree.processPayment);
-
-
-	var Wish = require('./controllers/wish');
-	app.post(PATH +'wish', Wish.AddWish);
-	app.get(PATH +'wish/fulfiller/:fulfillerID', Wish.findWishesFromFulfiller);
-	app.get(PATH +'wish/user/:userID', Wish.findWishesFromUser);
-	app.get(PATH +'wish', Wish.findAll);
-	app.get(PATH +'wish/PaidWishes', Wish.findPaidWishes);
-	app.put(PATH +'wish/:wishID', Wish.updateWish);
-	app.get(PATH +'wish/:wishID', Wish.findWish);	
-
-
+    var Wish = require('./controllers/wish');
+    app.post(PATH + 'wish', Wish.AddWish);
+    app.get(PATH + 'wish/fulfiller/:fulfillerID', Wish.findWishesFromFulfiller);
+    app.get(PATH + 'wish/user/:userID', Wish.findWishesFromUser);
+    app.get(PATH + 'wish', Wish.findAll);
+    app.get(PATH + 'wish/PaidWishes', Wish.findPaidWishes);
+    app.put(PATH + 'wish/:wishID', Wish.updateWish);
+    app.get(PATH + 'wish/:wishID', Wish.findWish);
 
     var Donation = require('./controllers/donation');
-	app.post(PATH +'donation', Donation.addDonation);
-	app.get(PATH +'donation', Donation.findAll);
-	app.put(PATH +'donation/:donationID', Donation.updateDonation);
-	app.get(PATH +'donation/:donationID', Donation.findDonation);
-	
-	app.get(PATH +'donation/user/:userID', Donation.findDonationsFromUser);
-	app.get(PATH +'donation/charity/:charityID', Donation.findDonationsFromCharity);
-	app.get(PATH +'donation/wish/:wishID', Donation.findDonationForWish);
+    app.post(PATH + 'donation', Donation.addDonation);
+    app.get(PATH + 'donation', Donation.findAll);
+    app.put(PATH + 'donation/:donationID', Donation.updateDonation);
+    app.get(PATH + 'donation/:donationID', Donation.findDonation);
 
+    app.get(PATH + 'donation/user/:userID', Donation.findDonationsFromUser);
+    app.get(PATH + 'donation/charity/:charityID', Donation.findDonationsFromCharity);
+    app.get(PATH + 'donation/wish/:wishID', Donation.findDonationForWish);
 
-	var Charity = require('./controllers/charity');
-	app.post(PATH +'charity', Charity.add);
-	app.get(PATH +'charity', Charity.findAll);
-	app.put(PATH +'charity/:charityID', Charity.updateCharity);
-	app.get(PATH +'charity/:charityID', Charity.findCharity);
+    var Charity = require('./controllers/charity');
+    app.post(PATH + 'charity', Charity.add);
+    app.get(PATH + 'charity', Charity.findAll);
+    app.put(PATH + 'charity/:charityID', Charity.updateCharity);
+    app.get(PATH + 'charity/:charityID', Charity.findCharity);
 
-	// console.log('done with routes'); 
+    // console.log('done with routes');
 
-	var User = require('./controllers/user');
-	// get all users
-	app.get(PATH + 'user', User.findAll);
+    var User = require('./controllers/user');
+    // get all users
+    app.get(PATH + 'user', User.findAll);
 
-	//this will take the access token and return the userID. 
-	//meant for testing only
-	app.get(PATH + 'user/auth', User.getUserID);
-	// get user object by UserID
-	app.get(PATH + 'user/:userID', User.getProfile);
-	// update a user by UserID 
-	app.put(PATH + 'user/:userID', User.updateProfile);
+    //this will take the access token and return the userID.
+    //meant for testing only
+    app.get(PATH + 'user/auth', User.getUserID);
+    // get user object by UserID
+    app.get(PATH + 'user/:userID', User.getProfile);
+    // update a user by UserID
+    app.put(PATH + 'user/:userID', User.updateProfile);
 
-	//test
-	app.get('/flash', function(req, res){
-		console.log('flash');
-  // Set a flash message by passing the key, followed by the value, to req.flash().
-	  req.flash('info', 'Flash is back!')
-	  res.redirect('/');
-	});
-	
-
-// app.put(baseURL + '/companies/:id', Companies.update);
-	// server.post({path : PATH +'GetDonations', version: versionNo}, GetDonations);
-	// server.post({path : PATH +'Fulfillments', version: versionNo}, Fulfillments);
-	// server.post({path : PATH +'GetPaidWishes', version: versionNo}, GetPaidWishes);
-	// server.post({path : PATH +'UpdateWishAsPaid', version: versionNo}, UpdateWishAsPaid);
-
-}
+    //test
+    app.get('/flash', function(req, res) {
+        console.log('flash');
+        // Set a flash message by passing the key, followed by the value, to req.flash().
+        req.flash('info', 'Flash is back!');
+        res.redirect('/');
+    });
+};
